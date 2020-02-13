037876115c3919592ae9207b383481fcd59084f8
Popescu-Iulian <popescu.iulian@protonmail.com>
2020-02-13 02:31:43 +0200

    final js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const TABEL = document.querySelector('table');
+const BODY = document.querySelector('body');
+const TABLE = document.createElement('TABLE');
 const BROWSER_COLORS = document.getElementById('browser-colors');
 const USER_COLORS = document.getElementById('user-colors');
 
 function drawTable(idx) {
+  BODY.appendChild(TABLE);
+  TABLE.setAttribute('border', '1');
+
   BROWSER_COLORS.value = '';
   let elements = '';
 
   for (let i = 0; i < idx; i++) {
     elements += `<tr>`;
+
     for (let j = 0; j < idx; j++) {
       elements += `<td onclick="applyColor(event)"></td>`
     }
+
     elements += `</tr>`
   }
 
-  TABEL.innerHTML = elements;
+  TABLE.innerHTML = elements;
+
+  clearUserColors();
 }
 
 function applyColor(event) {
@@ -26,5 +34,7 @@ function applyColor(event) {
 }
 
 function clearUserColors() {
-  USER_COLORS.value = '';
+  if (USER_COLORS.value !== '') {
+    USER_COLORS.value = '';
+  }
 }
